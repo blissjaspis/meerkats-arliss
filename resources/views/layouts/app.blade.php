25a34365b075548e09ad8d3ec4904e810b3c90fd
Bliss Jaspis <jaspis_zzt@ymail.com>
2026-04-23 14:23:43 +0700

    Remove unused JavaScript files and update Vite configuration to only include CSS input.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4/dist/css/splide.min.css">
         <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4/dist/js/splide.min.js"></script>
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css'])
         @livewireStyles
     </head>
     <body class="font-poppins antialiased">
